ee5552cdfee07597aa59b99613ba4fc122c27274
jhroelink <jannes.roelink@gmail.com>
2025-09-25 14:42:35 +0200

    rjktnj

--- src/test/java/com/example/devopstraining/controller/HelloControllerTest.java
+++ src/test/java/com/example/devopstraining/controller/HelloControllerTest.java
@@ -3,18 +3,24 @@ package com.example.devopstraining.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@SpringBootTest
 @WebMvcTest(HelloController.class)
 class HelloControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    private final MockMvc mockMvc;
 
+    @Autowired
+    HelloControllerTest(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
     @Test
     void testHello() throws Exception {
         mockMvc.perform(get("/api/hello"))
