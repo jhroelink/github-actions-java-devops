86772ef812fa6e5e15d9d706e9c0be3056a08a57
jhroelink <jannes.roelink@gmail.com>
2025-09-25 14:10:17 +0200

    rewtj

--- src/main/java/com/example/devopstraining/DevopsTrainingApplication.java
+++ src/main/java/com/example/devopstraining/DevopsTrainingApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DevopsTrainingApplication {
+public final class DevopsTrainingApplication {
 
-    public static void main(String[] args) {
+    private static void main(String[] args) {
         SpringApplication.run(DevopsTrainingApplication.class, args);
     }
 }
