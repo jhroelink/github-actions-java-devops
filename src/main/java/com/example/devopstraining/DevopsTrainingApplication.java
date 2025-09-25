09620f423022dc8f1a7f4cfd7df2eba1131da1dd
jhroelink <jannes.roelink@gmail.com>
2025-09-25 14:17:10 +0200

    dsfwebh

--- src/main/java/com/example/devopstraining/DevopsTrainingApplication.java
+++ src/main/java/com/example/devopstraining/DevopsTrainingApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DevopsTrainingApplication {
 
-    private static void main(final String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(DevopsTrainingApplication.class, args);
     }
 }
